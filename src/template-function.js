e85fe37d5f2cb20a8db469f2058f56b81a8a7433
Ngan Nguyen <ngancamnguyen@gmail.com>
2019-01-16 13:36:47 -0800

    setup

---
+++ src/template-function.js
@@ -0,0 +1,9 @@
+function age(dob) {
+  let today = new Date();
+  let birthDate = new Date(dob);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  // let month = today.getMonth() - birthDate.getMonth();
+  return age;
+}
+
+export { age };
