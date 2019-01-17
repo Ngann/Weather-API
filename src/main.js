e3c8ab22bcd479ea1887195367c3893545b42d1c
Student <example@example.com>
2019-01-16 18:44:55 -0800

    ok this is all working now

--- src/main.js
+++ src/main.js
@@ -3,26 +3,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Dino } from './../src/dino.js';
-// import { WeatherService } from './../src/weather-service.js';
+import { WeatherService } from './../src/weather-service.js';
 
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
+    let key = process.env.API_KEY
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e41268d4aaef10837c5c4b3b27b60744`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.");
-      }
+
+
+    let weatherService = new WeatherService();  // create instance of WeatherService class
+    let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
   });
+
 });
