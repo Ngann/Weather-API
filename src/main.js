acea733b677b86a4d671a2a801de31480ef5b700
Student <example@example.com>
2019-01-16 18:29:43 -0800

    correct API setup with UI and process.env

--- src/main.js
+++ src/main.js
@@ -2,7 +2,27 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Dino } from './../src/dino.js';
+// import { WeatherService } from './../src/weather-service.js';
+
 
 $(document).ready(function() {
-
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e41268d4aaef10837c5c4b3b27b60744`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
 });
--- src/template-function.js
+++
@@ -1,9 +0,0 @@
-function age(dob) {
-  let today = new Date();
-  let birthDate = new Date(dob);
-  let age = today.getFullYear() - birthDate.getFullYear();
-  // let month = today.getMonth() - birthDate.getMonth();
-  return age;
-}
-
-export { age };
