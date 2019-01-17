0cc72ff98e68cb9fdd46d4817362ea4bf0c20be6
Student <example@example.com>
2019-01-16 19:53:49 -0800

    add bike index info

--- src/dino.js
+++ src/dino.js
@@ -1,21 +1,22 @@
-export class Dino {
-// Vanilla JavaScript example
-  getDinoLorem() {
-    return new Promise(function(resolve, reject){
-      let xhr = new XMLHttpRequest();
-      let url = 'http://dinoipsum.herokuapp.com/api/?format=html&words=10&paragraphs=3';
-      xhr.onload = function(){
-        if (this.status === 200) {
-          resolve(document.querySelector('#some-awesome-container').innerHTML = this.response);
-        } else {
-          reject(Error(xhr.statusText));
-        }
-      }
-      xhr.open("GET", url, true);
-      xhr.send();
-    });
-  }
-}
+// export class Dino {
+// // Vanilla JavaScript example
+//   getDinoLorem() {
+//     return new Promise(function(resolve, reject){
+//       let xhr = new XMLHttpRequest();
+//       let url = 'http://dinoipsum.herokuapp.com/api/?format=html&words=10&paragraphs=3';
+//       xhr.onload = function(){
+//         if (this.status === 200) {
+//           resolve(document.querySelector('#some-awesome-container').innerHTML = this.response);
+//           // resolve(xhr.response);
+//         } else {
+//           reject(Error(xhr.statusText));
+//         }
+//       }
+//       xhr.open("GET", url, true);
+//       xhr.send();
+//     });
+//   }
+// }
 
 
 // Vanilla JavaScript example
--- src/main.js
+++ src/main.js
@@ -2,37 +2,51 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Dino } from './../src/dino.js';
-import { WeatherService } from './../src/weather-service.js';
+// import { Dino } from './../src/dino.js';
+// import { WeatherService } from './../src/weather-service.js';
+import { Bike } from './../src/bike.js';
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let key = process.env.API_KEY
-    let city = $('#location').val();
-    $('#location').val("");
+  //BIKE INDEX //
+  let bikeServices = new Bike();
+  let promise = bikeServices.getBikeInfor();
 
-
-    let weatherService = new WeatherService();  // create instance of WeatherService class
-    let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
+  promise.then(function(response) {
+    let bikeBody = JSON.parse(response);
+     $('.bikeName').text(`Bike brand ${bikeBody.manufacturer.name}`);
+     $('.bikeId').text(`Bike Id ${bikeBody.manufacturer.id}`);
+  }, function(error) {
+    $('.bikeErrors').text(`There was an error processing your request: ${error.message}`);
   });
+
+  /// Weather Services ///
+  // $('#weatherLocation').click(function() {
+  //   let key = process.env.API_KEY
+  //   let city = $('#location').val();
+  //   $('#location').val("");
+  //
+  //
+  //   let weatherService = new WeatherService();  // create instance of WeatherService class
+  //   let promise = weatherService.getWeatherByCity(city);  // call the instance method and pass in user input
+  //
+  //   promise.then(function(response) {
+  //     let body = JSON.parse(response);
+  //     $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+  //     $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+  //   }, function(error) {
+  //     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  //   });
+  // });
 
 
 ///// DINO ///
-  let dinoServices = new Dino();
-  let promise = dinoServices.getDinoLorem();
+  // let dinoServices = new Dino();
+  // let promise = dinoServices.getDinoLorem();
+  //
+  // promise.then(function(response) {
+  //   let dinoBody = JSON.parse(response);
+  // }, function(error) {
+  //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  // });
 
-  promise.then(function(response) {
-    let dinoBody = JSON.parse(response);
-    // $('#some-awesome-container').text(dinoBody);
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  });
 });
